1fb47252d1f5a259975d9999c15dfe98f2b4d4c9
vipul <harshvipul.1704@gmail.com>
2023-11-06 18:10:38 +0530

    Added login signup page

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Background from "./components/Background";
+import About from "./components/About";
+import Crausel from "./components/Crausel";
+import JoinTeam from "./components/JoinTeam";
+import Footer from "./components/Footer";
+import AboutUs from "./AboutUs";
+import HomePage from "./Pages/HomePage";
+import CertificatePage from "./Pages/CertificatePage";
+import News from "./Pages/News";
+import LoginRegister from "./components/LoginRegister";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route index element={<HomePage />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/certificate" element={<CertificatePage />} />
+            <Route path="/Home" element={<HomePage />} />
+            <Route path="/news" element={<News />} />
+<Route path="/login" element={<LoginRegister/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Certificates.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "./NavBar";
+import "./NavBar.css"
+const Certificates = () => {
+  return (
+<>
+
+    <div>
+      <h1>OUR CERTIFICATES</h1>
+      <div className="certi">
+        <div className="box2"></div>
+        <div className="box3"></div>
+        <div className="box4"></div>
+      </div>
+    </div>
+
+</>  
+);
+};
+
+export default Certificates;
